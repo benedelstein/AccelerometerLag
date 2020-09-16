a011ea8b97627a408f18cfbc192bbaed42750e89
Ben Edelstein <bedelstein12@gmail.com>
2020-09-15 21:52:34 -0500

    Initial Commit

---
+++ AccelerometerLagTest/AccelerometerLagTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccelerometerLagTestApp.swift
+//  AccelerometerLagTest
+//
+//  Created by Ben Edelstein on 9/15/20.
+//
+
+import SwiftUI
+
+@main
+struct AccelerometerLagTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccelerometerLagTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AccelerometerLagTest
+//
+//  Created by Ben Edelstein on 9/15/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
