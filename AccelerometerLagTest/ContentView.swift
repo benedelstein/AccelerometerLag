95992d75ab5fbf4bca76ac19e57ae526bd1f6d46
Ben Edelstein <bedelstein12@gmail.com>
2020-09-16 21:56:31 -0500

    fine tune variables

--- AccelerometerLagTest/ContentView.swift
+++ AccelerometerLagTest/ContentView.swift
@@ -9,20 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject var motionManager = MotionManager()
-    private let xyMultiplier: Double = 300
+    private let xyMultiplier: Double = 400
+    private let maxXY: Double = 100 // max pts to move over in xy plane
     private let zMultiplier: Double = 2
+    private let minAcceleration: Double = 0.0 // minimum acceleration to trigger a shift
     
     var body: some View {
         RoundedRectangle(cornerRadius: 10)
             .frame(width: 200, height: 200)
             .foregroundColor(.pink)
             .shadow(radius: 10)
-            .scaleEffect(CGFloat(1 + zMultiplier * motionManager.z))
-            .offset(x: CGFloat(motionManager.x * xyMultiplier), y: CGFloat(-motionManager.y * xyMultiplier))
+            .scaleEffect(abs(motionManager.z) > minAcceleration ? CGFloat(1 + zMultiplier * motionManager.z) : 1.0)
+            .offset(
+                x: abs(motionManager.x) > minAcceleration ? CGFloat(clipped(value: motionManager.x * xyMultiplier, maxValue: maxXY)) : 0,
+                y: abs(motionManager.y) > minAcceleration ? CGFloat(clipped(value: -motionManager.y * xyMultiplier, maxValue: maxXY)) : 0
+            )
             .animation(.spring())
             .onDisappear {
                 motionManager.stopUpdates()
             }
+    }
+    
+    private func clipped(value: Double, maxValue: Double) -> Double {
+        return max(min(value, maxValue), -maxValue)
     }
 }
 
--- AccelerometerLagTest/MotionManager.swift
+++ AccelerometerLagTest/MotionManager.swift
@@ -33,22 +33,6 @@ class MotionManager: ObservableObject {
                 self.z = userAccel.z
                 self.magnitude = self.magnitude(from: userAccel)
                 print(userAccel)
-                // set the reference attitude the first time
-//                if self.referenceAttitude == nil {
-//                    self.referenceAttitude = deviceData.attitude
-//                    // on the first pass, don't set any values since
-//                    // 1. they'll all be zero (by definition of reference)
-//                    // 2. referenceAttitude is a reference to the current deviceData object, and if we change it by multiplying it by itself we get reference = zero (flat on table), which is not desired
-//                    // then on the next update the attitude delta will not be accurate
-//                } else {
-//                    // get the relative attitude from the reference
-//                    deviceData.attitude.multiply(byInverseOf: self.referenceAttitude!)
-//                    self.magnitude = self.magnitude(from: deviceData.attitude)
-//                    self.x = deviceData.attitude.pitch / self.magnitude
-//                    self.y = deviceData.attitude.roll / self.magnitude
-//                    self.z = deviceData.attitude.yaw / self.magnitude
-//                    print("Roll: \(self.degrees(deviceData.attitude.roll)), Pitch: \(self.degrees(deviceData.attitude.pitch)), Yaw: \(self.degrees(deviceData.attitude.yaw))")
-//                }
             }
         }
     }
